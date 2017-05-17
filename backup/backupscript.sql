dc4d1792b013f2398e2f7697f0dce9bb7cc158ed
Anthony Pichardo <anthony.pichardo.453@my.csun.edu>
2017-05-17 16:09:24 -0700

    Update backupscript.sql

--- backup/backupscript.sql
+++ backup/backupscript.sql
@@ -1,2 +1,3 @@
+--database backup script
 BACKUP DATABASE [s17guest46] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL11.CSDB440\MSSQL\Backup\s17guest46.bak' WITH NOFORMAT, NOINIT,  NAME = N's17guest46-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
 GO
